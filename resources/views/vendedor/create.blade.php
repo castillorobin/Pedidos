f7c587d38a7f9c03d431407fa35afcc5176021c8
castillorobin <robin.castillo@catolica.edu.sv>
2022-11-21 15:51:50 -0600

    cambios en comercios

--- resources/views/vendedor/create.blade.php
+++ resources/views/vendedor/create.blade.php
@@ -10,30 +10,64 @@
 @section('content')
 
 <h2>Agregar Comercio</h2>
-
+<br>
+<div class="container ">
 <form action="/vendedores" method="POST">
     @csrf
-    <div class="mb-3 col-6" >
-        <label for="" class="form-label">Nombre</label>
-        
-        <input type="text" id="nombre" name="nombre" class="form-control" tabindex="1">
+  <div class="row border" style="background-color: white; padding: 15px; border-radius:20px;">
+   
+    <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">NOMBRE</label>
+
+    <div class="col-sm-8">
+    <input type="text" id="nombre" name="nombre" class="form-control" tabindex="1" placeholder="NOMBRE DEL COMERCIO" >
     </div>
-    <div class="mb-3 col-6">
-        <label for="" class="form-label">Direccion</label>
-        <input type="text" id="dire" name="dire" class="form-control" tabindex="2">
     </div>
-    <div class="mb-3 col-6">
-        <label for="" class="form-label">Telefono</label>
-        <input type="text" id="tele" name="tele" class="form-control" tabindex="3">
+    
+    <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">DIRECCION</label>
+
+    <div class="col-sm-8">
+    <input type="text" id="dire" name="dire" class="form-control" tabindex="1" placeholder="DIRECCION" >
+    </div>
+    </div>
+    
+    <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">TELEFONO</label>
+    
+    <div class="col-sm-3">
+    <input type="text" id="tele" name="tele" class="form-control" tabindex="3" placeholder="TELEFONO">
+    </div>
+    <label for="inputEmail3" class="col-sm-2 col-form-label">WHATSAPP</label>
+    <div class="col-sm-3">
+    <input type="text" id="what" name="what" class="form-control" tabindex="3" placeholder="WHATSAPP">
     </div>
 
-    <div class="mb-3 col-3">
-        <label for="" class="form-label">Fecha de ingreso</label>
-        <input type="date" id="fech" name="fech" class="form-control" tabindex="4">
     </div>
-    <div class="mb-3 col-3">
-        <label for="" class="form-label">Tipo de vendedor</label>
-        <select id="tipo" name="tipo" aria-label="Select a Timezone"  class="form-select ">
+   
+
+    <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">LONGITUD</label>
+    
+    <div class="col-sm-3">
+    <input type="text" id="lon" name="lon" class="form-control" tabindex="3" placeholder="LONGITUD">
+    </div>
+    <label for="inputEmail3" class="col-sm-2 col-form-label">LATITUD</label>
+    <div class="col-sm-3">
+    <input type="text" id="lat" name="lat" class="form-control" tabindex="3" placeholder="LATITUD">
+    </div>
+
+    </div>
+   
+    <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">FECHA DE INGRESO</label>
+    
+    <div class="col-sm-3">
+    <input type="date" id="fech" name="fech" class="form-control" tabindex="1"  >
+    </div>
+    <label for="inputEmail3" class="col-sm-2 col-form-label">TIPO DE VENDEDOR</label>
+    <div class="col-sm-3">
+    <select id="tipo" name="tipo" aria-label="Select a Timezone"  class="form-select ">
 			<option value="Corriente">Seleccionar tipo</option>		
 			<option value="Pequeño">Pequeño</option>
             <option value="Grande">Grande</option>
@@ -42,46 +76,125 @@
 		</select>
     </div>
 
-    <div class="mb-3 col-3">
-        <label for="" class="form-label"><strong> Datos Bancarios</strong></label>
-        
-    </div>
-    <div class="mb-3 col-3">
-        <label for="" class="form-label">Nombre del Titular</label>
-        <input type="text" id="titular" name="titular" class="form-control" tabindex="6" >
-    </div>
-    <div class="mb-3 col-3">
-        <label for="" class="form-label">Número de cuenta</label>
-        <input type="text" id="cuenta" name="cuenta" class="form-control" tabindex="7">
     </div>
 
-    <div class="mb-3 col-3">
-        <label for="" class="form-label">Banco</label>
-        <select id="banco" name="banco" aria-label="Select a Timezone"  class="form-select ">
-			<option value="sele">Seleccionar Banco</option>		
-			<option value="Davivienda">Davivienda</option>
-			<option value="Agricola">Agricola</option>
-			<option value="America Central">America Central</option>
-			<option value="Cuscatlan">Cuscatlan</option>
-		</select>
+
+    <div class="row mb-3">
+    
+
+    <div class="col-sm-8">
+    
+    </div>
     </div>
 
-    <div class="mb-3 col-3">
-        <label for="" class="form-label">Tipo de cuenta</label>
-        <select id="tipoc" name="tipoc" aria-label="Select a Timezone"  class="form-select ">
-			<option value="Corriente">Seleccionar tipo</option>		
-			<option value="Ahorro">Ahorro</option>
-            <option value="Corriente">Corriente</option>
-			<option value="Agricola">Otra</option>
+
+
+
+  </div>
+  <br>
+  <div class="row border" style="background-color: white; padding: 15px; border-radius:20px;">
+  <label for="" class="form-label"><strong> DATOS BANCARIOS</strong></label>
+
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">NOMBRE DEL TITULAR</label>
+
+    <div class="col-sm-8">
+    <input type="text" id="titular" name="titular" class="form-control" tabindex="1" placeholder="TITULAR" >
+    </div>
+    </div>
+
+    <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">NUMERO DE CUENTA</label>
+
+    <div class="col-sm-8">
+    <input type="text" id="cuenta" name="cuenta" class="form-control" tabindex="1" placeholder="# DE CUENTA" >
+    </div>
+    </div>
+
+    <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">TIPO DE CUENTA</label>
+
+    <div class="col-sm-8">
+    <select id="tipoc" name="tipoc" aria-label="Select a Timezone"  class="form-select ">
+			<option value="sele">SELECCIONAR TIPO</option>		
+			<option value="Davivienda">AHORRO</option>
+			<option value="Agricola">CORRIENTE</option>
 			
 		</select>
     </div>
-   
-    <br>
+    </div>
+
+
+</div>
+
+
+<br>
+  <div class="row border" style="background-color: white; padding: 15px; border-radius:20px;">
+  <label for="" class="form-label"><strong> DATOS CHIVO WALLET</strong></label>
+
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">NOMBRE</label>
+
+    <div class="col-sm-8">
+    <input type="text" id="nchivo" name="nchivo" class="form-control" tabindex="1" placeholder="NOMBRE" >
+    </div>
+    </div>
+
+    <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">DUI</label>
+    
+    <div class="col-sm-3">
+    <input type="text" id="dchivo" name="dchivo" class="form-control" tabindex="3" placeholder="DUI">
+    </div>
+    <label for="inputEmail3" class="col-sm-2 col-form-label">TELEFONO</label>
+    <div class="col-sm-3">
+    <input type="text" id="tchivo" name="tchivo" class="form-control" tabindex="3" placeholder="TELEFONO">
+    </div>
+
+    </div>
+
+</div>
+
+
+<br>
+  <div class="row border" style="background-color: white; padding: 15px; border-radius:20px;">
+  <label for="" class="form-label"><strong> DATOS TIGO MONEY</strong></label>
+
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">NOMBRE</label>
+
+    <div class="col-sm-8">
+    <input type="text" id="ntigo" name="ntigo" class="form-control" tabindex="1" placeholder="NOMBRE" >
+    </div>
+    </div>
+
+    <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">DUI</label>
+    
+    <div class="col-sm-3">
+    <input type="text" id="dtigo" name="dtigo" class="form-control" tabindex="3" placeholder="DUI">
+    </div>
+    <label for="inputEmail3" class="col-sm-2 col-form-label">TELEFONO</label>
+    <div class="col-sm-3">
+    <input type="text" id="ttigo" name="ttigo" class="form-control" tabindex="3" placeholder="TELEFONO">
+    </div>
+
+    </div>
+
+</div>
+ 
+<br>
    <button type="submit" class="btn btn-primary">Guardar</button>
 
 
 </form>
+</div>
+
+
+   
+
+   
+  
 
 
 @endsection
