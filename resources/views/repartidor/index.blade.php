763bd328eeaa8125d81e0fc4f815c08b64fda3d9
JOS394 <joeespana94@gmail.com>
2023-02-13 00:09:48 -0600

    00.09 13/02/2023

--- resources/views/repartidor/index.blade.php
+++ resources/views/repartidor/index.blade.php
@@ -13,7 +13,8 @@
 {{-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css"/> --}}
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css"/>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.dataTables.min.css"/>
-
+<link rel="stylesheet" href="https://kit.fontawesome.com/b64093b700.css" crossorigin="anonymous">
+ 
 
 
 
@@ -22,13 +23,14 @@
 
 
 <h1>Listado de Repartidores</h1>
-<div ><a href="/repartidores/create" class="btn btn-primary float-right" > Agregar Repartidor</a></div>
+<div ><a href="/repartidores/create" data-toggle="modal" data-target="#addrepartidor" class="btn btn-primary float-right" > Agregar Repartidor</a></div>
 <br>
 {{-- <table id="trepartidor" class="display responsive nowrap" style="width:100%"> --}}
   <div class="table-responsive">
 <table id="trepartidor" class="table table-bordered table-striped display responsive nowrap" style="width:100%">
 <thead class="table-dark">
     <tr >
+      <th></th>
       <th>Acciones</th>
       <th scope="col">:ID</th>
         <th scope="col">Nombre:</th>
@@ -58,21 +60,21 @@
 <tbody>
     @foreach ($repartidores as $repartidor)
     <tr data-id="{{ $repartidor->id  }}">
-
+        <td></td>
       <td>
         <div style="display:inline-flex;">
     <form action="{{ route ('repartidor.modal', $repartidor->id)}}" method="POST">
     @csrf
     @method('GET')
-    <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalVerMasData">
-        +
-      </button>                                                                     
+    {{-- <button  type="button" class="btn btn-success rounded-circle" data-toggle="modal" data-target="#modalVerMasData">
+      <b> + </b>
+      </button>                                                                      --}}
 </form>
         <form action="{{ route ('repartidores.destroy', $repartidor->id)}}" method="POST">
-        <a href="/repartidores/{{ $repartidor->id }}/edit" class="btn btn-primary">E</a>
+        <a href="/repartidores/{{ $repartidor->id }}/edit" class="btn btn-primary rounded-circle"><i class="fa-regular fa-pen-to-square"></i></a>
         @csrf
         @method('DELETE')
-        <button class="btn btn-danger">B</button>
+        <button class="btn btn-danger rounded-circle"><i class="fa-solid fa-trash"></i></button>
         </form>
         </div>
     </td>
@@ -106,106 +108,41 @@
 
 {{-- aqui empieza el modal --}}
 
-<div class="modal fade" id="modalVerMasData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Datos Repartidor</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <!-- Your table content goes here -->
-          <div class="table-responsive">
-            <table id="trepartidorModal" class="table table-bordered table-striped display responsive nowrap" style="width:100%">
-            <thead class="table-dark">
-                <tr >
-                  <th></th>
-                    <th scope="col">ID:</th>
-                    <th scope="col">Nombre:</th>
-                    <th scope="col">Direccion:</th>
-                     <th scope="col">Telefono:</th>
-                    <th scope="col">DUI:</th>
-                    <th scope="col">NIT:</th> 
-                    <th scope="col">tipo de contrato:</th>
-                    <th scope="col">agencia:</th>
-                    <th scope="col">numero de seguro:</th>
-                    <th scope="col">AFP:</th> 
-                    <th scope="col">cargo:</th>
-                    <th scope="col">fecha de alta:</th>
-                    <th scope="col">salario:</th>
-                    <th scope="col">fecha de baja:</th>
-                    <th scope="col">nota:</th>
-                    <th scope="col">tipo de vehiculo:</th>
-                    <th scope="col">asigno unidad:</th>
-                    <th scope="col">numero de placa:</th>
-                    <th scope="col">numero de tarjeta:</th>
-                    <th scope="col">numero de licencia:</th>
-                    <th scope="col">foto</th>
-                    <th scope="col">Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($repartidores as $repartidor)
-                <tr data-id="{{ $repartidor->id  }}">
-                  <td></td>
-                <td>{{ $repartidor->id }}</td>
-                <td>{{ $repartidor->nombre }}</td>
-                <td>{{ $repartidor->direccion }}</td>
-                <td>{{ $repartidor->telefono }}</td>
-                <td>{{ $repartidor->dui }}</td>
-                <td>{{ $repartidor->nit }}</td> 
-                <td>{{ $repartidor->tipo_contrato }}</td>
-                <td>{{ $repartidor->agencia }}</td>
-                <td>{{ $repartidor->num_seguro }}</td>
-                <td>{{ $repartidor->num_afp }}</td>
-                <td>{{ $repartidor->cargo }}</td>
-                <td>{{ $repartidor->fecha_de_alta }}</td>
-                <td>${{ $repartidor->salario }}.00</td>
-                <td>{{ $repartidor->fecha_de_baja }}</td>
-                <td>{{ $repartidor->nota }}</td>
-                <td>{{ $repartidor->tipo_vehiculo }}</td>
-                <td>{{ $repartidor->asigno_unidad }}</td>
-                <td>{{ $repartidor->num_placa }}</td>
-                <td>{{ $repartidor->num_tarjeta }}</td>
-                <td>{{ $repartidor->num_licencia }}</td> 
-                <td>{{ $repartidor->foto }}</td>    
-                <td>
-                    <div style="display:inline-flex;">
-                <form action="{{ route ('repartidor.modal', $repartidor->id)}}" method="POST">
-                @csrf
-                @method('GET')
-                <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalVerMasData">
-                    +
-                  </button>                                                                     
-            </form>
-                    <form action="{{ route ('repartidores.destroy', $repartidor->id)}}" method="POST">
-                    <a href="/repartidores/{{ $repartidor->id }}/edit" class="btn btn-primary">E</a>
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger">B</button>
-                    </form>
-                    </div>
-                </td>
-                </tr>
-                @endforeach
-            </tbody>
-            </table>
+<div class="modal fade" id="addrepartidor">
+  <div class="modal-dialog modal-dialog-full">
+    <div class="modal-content">
+      <form>
+        <div class="form-group">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+                <i class="fas fa-user"></i>
+              </span>
             </div>
-
+            <input type="text" class="form-control" placeholder="Username">
+          </div>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <div class="form-group">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+                <i class="fas fa-envelope"></i>
+              </span>
+            </div>
+            <input type="email" class="form-control" placeholder="Email">
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   </div>
+</div>
+
+
 
 
 
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-
+<script src="https://kit.fontawesome.com/b64093b700.js" crossorigin="anonymous"></script> 
 
 <script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js" defer></script>
 <script src="https://cdn.datatables.net/responsive/2.4.0/js/dataTables.responsive.min.js" defer></script>
