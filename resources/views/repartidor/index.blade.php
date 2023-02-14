7bb26886873f2841d5ced6ea64a35979ec25725a
JOS394 <joeespana94@gmail.com>
2023-02-13 23:45:24 -0600

    empezando front del agregar

--- resources/views/repartidor/index.blade.php
+++ resources/views/repartidor/index.blade.php
@@ -108,34 +108,70 @@
 
 {{-- aqui empieza el modal --}}
 
-<div class="modal fade" id="addrepartidor">
-  <div class="modal-dialog modal-dialog-full">
+
+
+<!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="addrepartidor" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
-      <form>
-        <div class="form-group">
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="fas fa-user"></i>
-              </span>
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      
+        <form>
+          <div class="form-group">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="fas fa-user"></i>
+                </span>
+              </div>
+              <input type="text" class="form-control" placeholder="Username">
             </div>
-            <input type="text" class="form-control" placeholder="Username">
           </div>
-        </div>
-        <div class="form-group">
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="fas fa-envelope"></i>
-              </span>
+          <div class="form-group">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="fas fa-trash"></i>
+                </span>
+              </div>
+              <input type="text" class="form-control" placeholder="Username">
             </div>
-            <input type="email" class="form-control" placeholder="Email">
           </div>
-        </div>
-      </form>
+
+
+          <div class="form-group">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="fas fa-envelope"></i>
+                </span>
+              </div>
+              <input type="email" class="form-control" placeholder="Email">
+            </div>
+          </div>
+        </form> 
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
     </div>
   </div>
 </div>
+
+
+
+
 
 
 
