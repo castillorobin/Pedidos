24f78948a7e58a77487d5b397df42073950ce699
castillorobin <robin.castillo@catolica.edu.sv>
2023-04-12 17:05:34 -0600

    cambios paginacion

--- resources/views/pedido/index.blade.php
+++ resources/views/pedido/index.blade.php
@@ -4,7 +4,7 @@
 @section('title', 'Melo Express')
 
 @section('content_header')
-    
+
     
 @stop
 
@@ -61,13 +61,16 @@ ul li ul:hover {
     }
 
 .cambiar2 {
-   
     float: right;
+    margin-right: 20px;
+    margin-top: 5px;
+   /*
+    
     margin-right: 300px;
     margin-bottom: 15px; 
     margin-top: -15px; 
     
-    
+    */
 }
 /*
 input[type="date"]::-webkit-calendar-picker-indicator {
@@ -104,25 +107,13 @@ input[type="date"]::-webkit-calendar-picker-indicator {
     margin-top: 0px;
     
 }
-.pagination{
+.dataTables_paginate a:hover {
+    color: white !important;
+    background:#0d6efd !important;
     
 }
-.paginate_button {
-    border-radius: solid 3px;
-}
 
-.dataTables_length select {
-    display: block;
-        background: transparent;
-       
-       
-        cursor: pointer;
-       
-        
-       
-      
-        width: auto;
-}
+
 
 input[type="date"]::-webkit-calendar-picker-indicator {
         display: block;
@@ -363,12 +354,17 @@ input[type="date"]:focus::before{
 	
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 
-    <script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js" defer></script>
-<script src="https://cdn.datatables.net/responsive/2.4.0/js/dataTables.responsive.min.js" defer></script>
 
-<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js" defer></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/cleave.js/1.6.0/cleave.min.js" ></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js" defer></script>
+\
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js" defer></script>
+   
 
+    <!--
+ <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js" defer></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js" defer></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js" defer></script>
+-->
 
 
 
@@ -384,8 +380,8 @@ input[type="date"]:focus::before{
            
         },
         
-        dom: 'B<"cambiar" f><"pagina2" p><"cambiar2"l>tri<"pagina1" p>',
-        responsive: true,
+        dom: '<"cambiar" f><"pagina2" p><"cambiar2"l>tri<"pagina1" p>',
+     
      
     
        
@@ -412,6 +408,14 @@ input[type="date"]:focus::before{
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-7mQhpDl5nRA5nY9lr8F1st2NbIly/8WqhjTp+0oFxEA/QUuvlbF6M1KXezGBh3Nb" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap.min.css" />
+    
+
+    <!--
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css" />
+-->
    
 @stop
